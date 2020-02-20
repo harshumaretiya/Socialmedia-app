36fff73662a19e69d840a5f31328c5dea8eed277
Harsh <harsh.patel.hp846@gmail.com>
2020-02-20 14:06:56 +0530

    last

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,22 @@ Rails.application.configure do
   # and use secure cookies.
   config.force_ssl = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
